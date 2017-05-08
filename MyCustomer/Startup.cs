0df8340335b0726a439edca4d3830b0f24ab8839
wjs0521 <wjs0521@gmail.com>
2017-05-08 22:21:57 +0800

    加入專案檔案。

---
+++ MyCustomer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyCustomer.Startup))]
+namespace MyCustomer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
